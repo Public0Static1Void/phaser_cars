e21fe8388969c818526de0b0b775cb78d666d0a8
Dani <dangp2003@gmail.com>
2024-05-31 15:57:35 +0200

    1.0

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let http = require('http');
 let ws = require('ws');
 
 let file = new static.Server('./public');
- 
+
 let http_server = http.createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
-})
+});
 
 let ws_server = new ws.WebSocketServer({ server: http_server });
 
@@ -20,114 +20,116 @@ let p2_conn;
 let curr_id = 0;
 
 var p1 = {
-	x: 400,
-	y: 300,
-	r: 0,
-	id: 1
+    x: 400,
+    y: 300,
+    r: 0,
+    id: 1,
+	game_over: ""
 };
 var p2 = {
-	x: 400,
-	y: 400,
-	r: 0,
-	id: 2
+    x: 400,
+    y: 400,
+    r: 0,
+    id: 2,
+	game_over: ""
 };
 var bull = {
-	bx: -100,
-	by: 0,
-	br: 0,
-	bo: 0
+    bx: -100,
+    by: 0,
+    br: 0,
+    bo: 0
 }
 
 let specs = [];
 let conn_users = 3;
 
-ws_server.on('connection', function (conn){
+let go = "";
 
-	console.log('EVENT: Connection');
+ws_server.on('connection', function (conn) {
+    console.log('EVENT: Connection');
 
-	if (p1_conn == undefined){
-	  	p1_conn = conn;
-	  	p1_conn.send('{"player_num":1}');
+    if (p1_conn == undefined) {
+        p1_conn = conn;
+        p1_conn.send('{"player_num":1}');
 
-      	p1_conn.on('message', function(data){
-			data = data.toString();
+        p1_conn.on('message', function (data) {
+            data = data.toString();
 
-			if (data.length <= 0)
-				return;
+            if (data.length <= 0)
+                return;
 
-			specs.forEach(viewer => {
-				viewer.send(data.toString());
-			});
+            specs.forEach(viewer => {
+                viewer.send(data.toString());
+            });
 
-			let dat = JSON.parse(data);
+            let dat = JSON.parse(data);
 
-			if (dat.bx != undefined) {
-				bull.bx = dat.bx;
-				bull.by = dat.by;
-				bull.br = dat.br;
-				bull.bo = dat.bo;
-			}else{
-				p1.x = dat.x;
-				p1.y = dat.y;
-				p1.r = dat.r;
-				p1.id = dat.id;
-			}
+            if (dat.bx != undefined) {
+                bull.bx = dat.bx;
+                bull.by = dat.by;
+                bull.br = dat.br;
+                bull.bo = dat.bo;
+            } else if (dat.x != undefined) {
+                p1.x = dat.x;
+                p1.y = dat.y;
+                p1.r = dat.r;
+                p1.id = dat.id;
+				p1.game_over = dat.game_over;
+            }
 
-			//console.log("P1: ", p1);
-			//console.log("P2: ", p2);
+            p1_conn.send(JSON.stringify(p1));
+            p1_conn.send(JSON.stringify(p2));
+            p1_conn.send(JSON.stringify(bull));
+        });
+    } else if (p2_conn == undefined) {
+        p2_conn = conn;
+        p2_conn.send('{"player_num":2}');
 
-			p1_conn.send(JSON.stringify(p1));
-			p1_conn.send(JSON.stringify(p2));
-			p1_conn.send(JSON.stringify(bull));
-		});
+        p2_conn.on('message', function (data) {
+            data = data.toString();
+            if (data.length <= 0)
+                return;
+
+            specs.forEach(viewer => {
+                viewer.send(data.toString());
+            });
+
+            let dat = JSON.parse(data);
+            if (dat.bx != undefined) {
+                bull.bx = dat.bx;
+                bull.by = dat.by;
+                bull.br = dat.br;
+                bull.bo = dat.bo;
+            } else if (dat.x != undefined) {
+                p2.x = dat.x;
+                p2.y = dat.y;
+                p2.r = dat.r;
+                p2.id = dat.id;
+				p2.game_over = dat.game_over;
+            }
+
+            p2_conn.send(JSON.stringify(p1));
+            p2_conn.send(JSON.stringify(p2));
+            p2_conn.send(JSON.stringify(bull));
+        });
+    } else {
+        let data = '{"player_num":' + conn_users + '}';
+        conn.send(data);
+
+        conn.send(JSON.stringify(p1));
+        conn.send(JSON.stringify(p2));
+        conn.send(JSON.stringify(bull));
+
+        conn.on('message', function (data) {
+            if (data.length <= 0) return;
+
+            if (go != "")
+                conn.send('{"game_over":"' + go + '"}');
+        });
+
+        console.log('Spectators: ' + (conn_users - 2));
+
+        conn_users++;
+        specs.push(conn);
     }
-	else if (p2_conn == undefined){
-	 	p2_conn = conn;
-	 	p2_conn.send('{"player_num":2}');
-
-     	p2_conn.on('message', function(data){
-			data = data.toString();
-			if (data.length <= 0)
-				return;
-
-			specs.forEach(viewer => {
-				viewer.send(data.toString());
-			});
-
-			let dat = JSON.parse(data);
-			if (dat.bx != undefined) {
-				bull.bx = dat.bx;
-				bull.by = dat.by;
-				bull.br = dat.br;
-				bull.bo = dat.bo;
-			}else{
-				p2.x = dat.x;
-				p2.y = dat.y;
-				p2.r = dat.r;
-				p2.id = dat.id;
-			}
-			
-
-			//console.log("P1: ", p1);
-			//console.log("P2: ", p2);
-
-			p2_conn.send(JSON.stringify(p1));
-			p2_conn.send(JSON.stringify(p2));
-			p2_conn.send(JSON.stringify(bull));
-		});
-   }
-   else {
-		let data = '{"player_num":' + conn_users + '}';
-		conn.send(data);
-
-		conn.send(JSON.stringify(p1));
-		conn.send(JSON.stringify(p2));
-		conn.send(JSON.stringify(bull));
-
-		console.log('Spectators: ' + (conn_users - 2));
-
-		conn_users++;
-		specs.push(conn);
-	}
-
 });
--- public/carmaenti.js
+++ public/carmaenti.js
@@ -1,46 +1,46 @@
 let player_num = 0;
 let id = 0;
 
-let player1;
-let player2;
+var player1 = { x: 0, y: 0, rotation: 0 };
+var player2 = { x: 0, y: 0, rotation: 0 };
+var bullet = { x: 0, y: 0, rotation: 0 };
 let map;
-let bullet;
 
 let bull_angle = 0;
 let bullet_objective = 0;
 
-let game_over = false;
+let game_over_text = "";
+
+let game;
+let config;
 
 const socket = new WebSocket("ws://192.168.1.154:8081");
 
-socket.addEventListener("open", function(event){
+socket.addEventListener("open", function(event) {
     socket.send("");
 });
 
-socket.addEventListener("message", function(event){
-    try{
-        // Ensure data is consistently parsed
+socket.addEventListener("message", function(event) {
+    try {
         var data = JSON.parse(event.data);
 
-        // Handle the initial assignment of player_num and id
         if (data.player_num !== undefined && id === 0) {
             player_num = data.player_num;
             console.log("Jugador: ", player_num);
             id = player_num;
         } else {
-            // Handle incoming player and bullet data
             if (data.player_num !== undefined) {
                 console.log("Jugador: " + data.player_num);
             } else {
-                // Update bullet position and rotation if provided
                 if (data.bx !== undefined) {
                     bullet.x = data.bx;
                     bullet.y = data.by;
                     bullet.rotation = data.br;
                     bullet_objective = data.bo;
                 }
-                
-                // Update player positions and rotations
+
+                if (data.x === undefined || data.y === undefined) return;
+
                 switch (data.id) {
                     case 1:
                         player1.x = data.x;
@@ -53,11 +53,13 @@ socket.addEventListener("message", function(event){
                         player2.rotation = data.r;
                         break;
                 }
-                
-                // Handle game over status if provided
-                if (data.game_over !== undefined) {
-                    game_over = data.game_over;
-                }
+                game_over_text = data.game_over;
+                if (game_over_text !== "" && game !== undefined)
+                    game.add.text(config.width / 3.5, config.height / 2.25, game_over_text, {
+                        fontFamily: 'Comfortaa, sans-serif',
+                        fontSize: 64,
+                        color: '#FF2AA'
+                    }).setVisible(true);
             }
         }
     } catch (error) {
@@ -65,18 +67,17 @@ socket.addEventListener("message", function(event){
     }
 });
 
-
-socket.addEventListener("error", function(event){
-    console.error("Websocket error:", event);
+socket.addEventListener("error", function(event) {
+    console.error("WebSocket error:", event);
 });
 
 let player1_angle = 0;
 let player2_angle = 0;
 
-document.addEventListener("DOMContentLoaded", function(){
-    const config = {
+document.addEventListener("DOMContentLoaded", function() {
+    config = {
         type: Phaser.AUTO,
-        width: 800, 
+        width: 800,
         height: 600,
         physics: {
             default: 'arcade',
@@ -87,7 +88,7 @@ document.addEventListener("DOMContentLoaded", function(){
         scene: {
             preload: preload,
             create: create,
-            update: update, 
+            update: update,
         }
     };
 
@@ -99,72 +100,77 @@ document.addEventListener("DOMContentLoaded", function(){
     let keys;
     let bullet_pressed = false;
 
-    function preload (){
+    function preload() {
         this.load.image('car1', 'assets/PNG/Cars/car_blue_small_1.png');
         this.load.image('car2', 'assets/PNG/Cars/car_green_small_1.png');
         this.load.image('map', 'assets/PNG/map.png');
         this.load.image('bullet', 'assets/PNG/bala.png');
     }
 
-    function create (){
+    function create() {
+        game = this;
         map = this.add.image(400, 300, 'map');
         player1 = this.physics.add.image(400, 300, 'car1');
         player2 = this.physics.add.image(400, 400, 'car2');
-    
+
         bullet = this.physics.add.image(-100, 0, 'bullet');
         bullet.setScale(.1);
 
         keys = this.input.keyboard.createCursorKeys();
 
         // Enable collision detection
-		switch(id){
-			case 1:
-				this.physics.add.collider(bullet, player2, hitPlayer, null, this);
-				break;
-			case 2:
-				this.physics.add.collider(bullet, player1, hitPlayer, null, this);
-				break;
-		}
+        if (id === 1) {
+            this.physics.add.collider(bullet, player2, hitPlayer, null, this);
+        } else if (id === 2) {
+            this.physics.add.collider(bullet, player1, hitPlayer, null, this);
+        }
     }
 
     function hitPlayer(bullet, player) {
         // Display message
-        switch(id){
-			case 1:
-				alert("Player 2 wins!");
-				break;
-			case 2:
-				alert("Player 1 wins!");
-				break;
-		}
+        switch (id) {
+            case 1:
+                game_over_text = "Player 1 wins!";
+                break;
+            case 2:
+                game_over_text = "Player 2 wins!";
+                break;
+        }
+        var final = {
+            game_over: game_over_text
+        };
+        
         // Reset bullet
         bullet.x = -50;
         bullet.y = 0;
         bullet_pressed = false;
     }
-    
+
+    function showGameOverText(text) {
+        
+    }
+
     var space_hold = false;
     let const_ang = 0;
 
-    function update (){
-        switch(id){
+    function update() {
+        switch (id) {
             case 1:
-                if(keys.up.isDown){
+                if (keys.up.isDown) {
                     player1.y -= CAR_SPEED * Math.cos(player1_angle * Math.PI / 180);
                     player1.x += CAR_SPEED * Math.sin(player1_angle * Math.PI / 180);
                 }
-                if(keys.down.isDown){
+                if (keys.down.isDown) {
                     player1.y += CAR_SPEED * Math.cos(player1_angle * Math.PI / 180);
                     player1.x -= CAR_SPEED * Math.sin(player1_angle * Math.PI / 180);
                 }
-                if(keys.left.isDown){
+                if (keys.left.isDown) {
                     player1_angle -= CAR_ROTATION;
-                }
-                else if (keys.right.isDown){
+                } else if (keys.right.isDown) {
                     player1_angle += CAR_ROTATION;
                 }
 
-                if (keys.space.isDown){ // Bala
+                if (keys.space.isDown) { // Bala
                     bullet.x = player1.x;
                     bullet.y = player1.y;
                     bullet.rotation = player1_angle * Math.PI / 180;
@@ -176,22 +182,21 @@ document.addEventListener("DOMContentLoaded", function(){
                 player1.rotation = player1_angle * Math.PI / 180;
                 break;
             case 2:
-                if(keys.up.isDown){
+                if (keys.up.isDown) {
                     player2.y -= CAR_SPEED * Math.cos(player2_angle * Math.PI / 180);
                     player2.x += CAR_SPEED * Math.sin(player2_angle * Math.PI / 180);
                 }
-                if(keys.down.isDown){
+                if (keys.down.isDown) {
                     player2.y += CAR_SPEED * Math.cos(player2_angle * Math.PI / 180);
                     player2.x -= CAR_SPEED * Math.sin(player2_angle * Math.PI / 180);
                 }
-                if(keys.left.isDown){
+                if (keys.left.isDown) {
                     player2_angle -= CAR_ROTATION;
-                }
-                else if (keys.right.isDown){
+                } else if (keys.right.isDown) {
                     player2_angle += CAR_ROTATION;
                 }
 
-                if (keys.space.isDown){ // Bala
+                if (keys.space.isDown) { // Bala
                     bullet.x = player2.x;
                     bullet.y = player2.y;
                     bullet.rotation = player2_angle * Math.PI / 180;
@@ -203,21 +208,23 @@ document.addEventListener("DOMContentLoaded", function(){
                 player2.rotation = player2_angle * Math.PI / 180;
                 break;
         }
-         
+
         var player1_data = {
             x: player1.x,
             y: player1.y,
             r: player1.rotation,
-            id: id
+            id: id,
+            game_over: game_over_text
         };
         var player2_data = {
             x: player2.x,
             y: player2.y,
             r: player2.rotation,
-            id: id
+            id: id,
+            game_over: game_over_text
         };
-     
-        switch(id){
+
+        switch (id) {
             case 1:
                 socket.send(JSON.stringify(player1_data));
                 break;
@@ -226,8 +233,8 @@ document.addEventListener("DOMContentLoaded", function(){
                 break;
         }
 
-        if (bullet_pressed){ // Send bullet
-            if (space_hold){
+        if (bullet_pressed) { // Send bullet
+            if (space_hold) {
                 const_ang = (bullet_objective === 1) ? player2_angle : player1_angle;
                 space_hold = false;
             }
